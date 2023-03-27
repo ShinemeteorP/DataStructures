8f2b0f9ca02ee10075b09f2165b180d4f77810b8
meteor <meteor@gamil.com>
2023-03-27 22:08:50 +0800

    ssh update

--- src/Git/Test.java
+++ src/Git/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.print( "welcome to git");
         System.out.print( "welcome to git test1");
+        System.out.print( "welcome to git test2");
     }
 }
